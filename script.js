199a445dbdbcbcf7550ec3385f95c73c9c991cd8
msp9685-commits <msp9685@gmail.com>
2026-08-15 13:36:02 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let buttons = document.querySelectorAll("button");
-buttons.forEach(function(button){
-    button.addEventListener("click", function(){
-        console.log("button clicked")
-    })
-});
